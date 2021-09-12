357fd7a01ea3e8fdbbe7fa8a15fb323de5900753
sameerion42 <sameermark42@gmail.com>
2021-09-12 20:07:26 +0530

    explanatory comments added

--- K_score.java
+++ K_score.java
@@ -1,3 +1,4 @@
+//K-Goodnes string
 import java.io.*;
 import java.util.*;
 import java.lang.Math;
@@ -14,24 +15,28 @@ public class K_score
         while(test-- > 0)
         {   
             int size = sc.nextInt();
+            //required K-score to reach
             int K_res = sc.nextInt();
+            //take up the '\n' character in input buffer
             String trash = sc.nextLine();
             String str = sc.nextLine();
-
+            //actual K-score of given string
             int K_act=0;
+            //counting the K_act goodness score
             for(int i=0;i <size/2;i++)
             {
                 if(str.charAt(i) != str.charAt(size -1 -i))
                     ++K_act;
             }
-
+            //if given score and required score are equal minimum steps is 0
             if(K_act == K_res)
                 System.out.println("Case #"+Case+": "+0);
             else
+                //difference of required score and acutal score give minimum no of steps
                 System.out.println("Case #"+Case+": "+Math.abs(K_res - K_act));
 
             ++Case;
         }
-        
     }
+    
 }
